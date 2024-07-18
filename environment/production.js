a28fb9e3b7713d2bcf83536f381d72c14735b4f5
sand Ratiaray <sratiaray@gmail.com>
2024-07-18 16:28:28 +0200

    add mongodb path

--- environment/production.js
+++ environment/production.js
@@ -1,9 +1,8 @@
 const path = require('path');
-const dotenv = require('dotenv').config();
 
 
 module.exports = {
-    dbUrl : process.env.MONGODB_URL,
+    dbUrl : 'mongodb+srv://sratiaray:No%40h19110715@cluster0.qxqlpuo.mongodb.net/twitter?retryWrites=true',
     cert:'/etc/letsencrypt/live/www.srtwit.site/fullchain.pem',
     key: '/etc/letsencrypt/live/www.srtwit.site/privkey.pem',
     portHttp:80,

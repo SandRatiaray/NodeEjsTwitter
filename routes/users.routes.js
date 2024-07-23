2edd928e440caf28235ec0c6956fadb9bc468083
sand Ratiaray <sratiaray@gmail.com>
2024-07-23 19:37:18 +0200

    add routes

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
-const { signup, signupForm, uploadImage, userList, userProfile } = require('../controller/users.controler');
+const { followUser, signup, signupForm, unfollowUser, uploadImage, userList, userProfile } = require('../controller/users.controler');
 const { ensureAuthenticated } = require('../config/guards.config');
 
 router.get('/', userList)
+router.get('/follow/:userId', followUser)
+router.get('/unfollow/:userId', unfollowUser)
 router.get('/:username', userProfile);
 router.get('/signup/form', signupForm);
 router.post('/signup', signup);
